b0fffb64174063312c34ad0bfabf5664f6838ca6
H4XOR_ <hakroot07@gmail.com>
2024-09-01 19:49:09 +0200

    Add rootCmd
    add rootCmd in main.go and initialize keys and signatures in man.go

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"simple_brave_signer/cmd"
+	"simple_brave_signer/cmd/keys"
+	"simple_brave_signer/cmd/signatures"
+)
 
+func main() {
+	rootCmd := cmd.RootCmd()
+	keys.Init(rootCmd)
+	signatures.Init(rootCmd)
 }
