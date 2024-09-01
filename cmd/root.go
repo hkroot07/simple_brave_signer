d71ded145fb84e1100f0435c93e2963d78dc80d8
H4XOR_ <hakroot07@gmail.com>
2024-09-01 19:25:08 +0200

    Create package 'cmd' and 'RootCmd'

---
+++ cmd/root.go
@@ -0,0 +1,11 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+func RootCmd() *cobra.Command {
+	return &cobra.Command{
+		Use:   "simple_brave_signer",
+		Short: "Bravely generate key pairs, sign file and check signatures.",
+		Long:  `A collection of tools to generate key pairs in PEM files, sign files and verify signatures.`,
+	}
+}
