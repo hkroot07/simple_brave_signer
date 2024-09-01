bc1e046022a93c19b1ec63a478cf9160207166fb
H4XOR_ <hakroot07@gmail.com>
2024-09-01 19:33:21 +0200

    Add packages 'keys' and 'signatures'
    Create keys.go

---
+++ cmd/keys/keys.go
@@ -0,0 +1,13 @@
+package keys
+
+import "github.com/spf13/cobra"
+
+var keysCmd = &cobra.Command{
+	Use:   "keys",
+	Short: "Manage key pairs.",
+	Long:  `Use subcommands to create public/private key pairs in PEM files.`,
+}
+
+func Init(rootCmd *cobra.Command) {
+	rootCmd.AddCommand(keysCmd)
+}
