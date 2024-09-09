20f93463763349a64b7a44d0f747c79a7ae81c0d
H4XOR_ <hakroot07@gmail.com>
2024-09-09 19:13:33 +0200

    Add function 'DeriveKey'

--- crypto_utils/crypto_utils.go
+++ crypto_utils/crypto_utils.go
@@ -1,1 +1,20 @@
 package cryptoutils
+
+import (
+	"fmt"
+
+	"golang.org/x/crypto/argon2"
+)
+
+type KeyDerivationConfig struct {
+	Passphrase []byte
+	Salt       []byte
+}
+
+func DeriveKey(config KeyDerivationConfig) ([]byte, error) {
+	if len(config.Passphrase) == 0 || len(config.Salt) == 0 {
+		return nil, fmt.Errorf("passphrase and salt  cannot be empty")
+	}
+
+	return argon2.IDKey(config.Passphrase, config.Salt, 1, 64*1024, 4, 32), nil
+}
