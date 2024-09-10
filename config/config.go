26d2750d2f21b6a571a8acc0094466dd2d1f8e00
H4XOR_ <hakroot07@gmail.com>
2024-09-10 17:42:30 +0200

    Add function 'LoadYamlConfig'

--- config/config.go
+++ config/config.go
@@ -1,1 +1,25 @@
 package config
+
+import (
+	"fmt"
+	"simple_brave_signer/logger"
+
+	"github.com/spf13/viper"
+)
+
+func LoadYamlConfig() (*viper.Viper, error) {
+	localViper := viper.New()
+	localViper.SetConfigName("config")
+	localViper.SetConfigType("yaml")
+	localViper.AddConfigPath(".")
+
+	err := localViper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			logger.Info("Config file not found, using CLI params or default settings...")
+		} else {
+			return localViper, fmt.Errorf("found config file, but encountered an error: %v", err)
+		}
+	}
+	return localViper, nil
+}
